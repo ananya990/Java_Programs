64c9faf6e4d70dfbcc48d6dd26328d5eebdd4c6d
ananya990 <142164935+ananya990@users.noreply.github.com>
2023-09-03 20:08:13 +0530

    Create Java_Program_to_Check_Whether_a_Number_is_Positive_or_Negative.java

---
+++ Java_Program_to_Check_Whether_a_Number_is_Positive_or_Negative.java
@@ -0,0 +1,16 @@
+public class PositiveNegative 
+{
+    public static void main(String[] args) 
+  {
+        double number = -1.1;
+    
+        if (number < 0.0)
+            System.out.println(number + " is a negative number.");
+
+        else if ( number > 0.0)
+            System.out.println(number + " is a positive number.");
+
+        else
+            System.out.println(number + " is 0.");
+    }
+}
