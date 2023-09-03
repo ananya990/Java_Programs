cca42a0646bc05540c999e3f83c2a1d05cd0e702
ananya990 <142164935+ananya990@users.noreply.github.com>
2023-09-03 15:58:57 +0530

    Create Java_Program_to_Add_Two_Integers.java

---
+++ Java_Program_to_Add_Two_Integers.java
@@ -0,0 +1,11 @@
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    int first = 10, second = 20;
+
+    int sum = first + second;
+    
+    System.out.println(first + " + " + second + " = "  + sum);
+  }
+}
