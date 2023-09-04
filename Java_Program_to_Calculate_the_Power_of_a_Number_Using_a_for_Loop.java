2f3ab2b4571ee352154b0fc8d5983a1737548e59
ananya990 <142164935+ananya990@users.noreply.github.com>
2023-09-04 18:54:04 +0530

    Create Java_Program_to_Calculate_the_Power_of_a_Number_Using_a_for_Loop.java

---
+++ Java_Program_to_Calculate_the_Power_of_a_Number_Using_a_for_Loop.java
@@ -0,0 +1,13 @@
+class PowerofNum 
+{
+  public static void main(String[] args) 
+  {
+    int base = 2, exponent = 5;
+    long result = 1;
+
+    for (; exponent != 0; --exponent) 
+        result *= base;
+
+    System.out.println("Answer = " + result);
+  }
+}
