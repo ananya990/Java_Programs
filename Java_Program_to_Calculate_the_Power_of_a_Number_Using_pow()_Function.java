a719b042129af879f51d6f78a43f4b0498f6b251
ananya990 <142164935+ananya990@users.noreply.github.com>
2023-09-04 19:03:20 +0530

    Create Java_Program_to_Calculate_the_Power_of_a_Number_Using_pow()_Function.java

---
+++ Java_Program_to_Calculate_the_Power_of_a_Number_Using_pow()_Function.java
@@ -0,0 +1,10 @@
+class PowerOfNum 
+{
+  public static void main(String[] args) 
+  {
+    int base = 2, exponent = -6;
+    double result = Math.pow(base, exponent);
+
+    System.out.println("Answer = " + result);
+  }
+}
