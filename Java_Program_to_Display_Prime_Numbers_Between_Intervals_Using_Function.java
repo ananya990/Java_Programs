f3c4bdaa6cb56a6c5c396b85f4fa029cc480a126
ananya990 <142164935+ananya990@users.noreply.github.com>
2023-09-06 20:49:30 +0530

    Create Java_Program_to_Display_Prime_Numbers_Between_Intervals_Using_Function.java

---
+++ Java_Program_to_Display_Prime_Numbers_Between_Intervals_Using_Function.java
@@ -0,0 +1,31 @@
+public class PrimeNos 
+{
+    public static void main(String[] args) 
+    {
+        int low = 10, high = 40;
+
+        while (low < high) 
+        {
+            if(checkPrimeNumber(low))
+                System.out.print(low + " ");
+
+            ++low;
+        }
+    }
+
+    public static boolean checkPrimeNumber(int num) 
+    {
+        boolean flag = true;
+
+        for(int i = 2; i <= num/2; ++i) 
+        {
+            if(num % i == 0) 
+            {
+                flag = false;
+                break;
+            }
+        }
+
+        return flag;
+    }
+}
