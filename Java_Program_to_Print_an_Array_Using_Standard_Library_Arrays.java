a8e921e08daea64b820c21b3b476adbecf2bc1c2
ananya990 <142164935+ananya990@users.noreply.github.com>
2023-09-19 01:20:49 +0530

    Create Java_Program_to_Print_an_Array_Using_Standard_Library_Arrays.java

---
+++ Java_Program_to_Print_an_Array_Using_Standard_Library_Arrays.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class DisplayArray 
+{
+    public static void main(String[] args) 
+    {
+        int[] array = {10, 20, 30, 40, 50, 60, 70};
+
+        System.out.println("Elements of array are : ");
+        System.out.println(Arrays.toString(array));
+    }
+}
