ad3d965c7910728b1424417a1b24a9e0b558265c
ananya990 <142164935+ananya990@users.noreply.github.com>
2023-09-19 01:19:08 +0530

    Create Java_Program_to_Print_an_Array_Using_for_Loop.java

---
+++ Java_Program_to_Print_an_Array_Using_for_Loop.java
@@ -0,0 +1,13 @@
+public class DisplayArray 
+{
+    public static void main(String[] args) 
+    {
+        int[] array = {10, 20, 30, 40, 50, 60, 70};
+
+        System.out.println("Elements of array are : ");
+        for (int element : array) 
+        {
+            System.out.println(element);
+        }
+    }
+}
