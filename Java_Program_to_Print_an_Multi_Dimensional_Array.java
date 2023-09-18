669b618d90a4fe33e4584c8d2eea829c7c722764
ananya990 <142164935+ananya990@users.noreply.github.com>
2023-09-19 01:22:34 +0530

    Create Java_Program_to_Print_an_Multi_Dimensional_Array.java

---
+++ Java_Program_to_Print_an_Multi_Dimensional_Array.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class DisplayArray 
+{
+    public static void main(String[] args) 
+    {
+        int[][] array = {{10, 20}, {30, 40}, {50, 60, 70}};
+
+        System.out.println(Arrays.deepToString(array));
+    }
+}
