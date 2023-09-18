cf40b1554bdb87352c9a80fbb0505ff46b92f8e4
ananya990 <142164935+ananya990@users.noreply.github.com>
2023-09-18 23:18:08 +0530

    Create Java_Program_to_Find_the_Frequency_of_Character_in_a_String.java

---
+++ Java_Program_to_Find_the_Frequency_of_Character_in_a_String.java
@@ -0,0 +1,19 @@
+public class FrequencyofaChar 
+{
+    public static void main(String[] args) 
+    {
+        String str = "C language is easy.";
+        char ch = 'a';
+        int frequency = 0;
+
+        for(int i = 0; i < str.length(); i++) 
+        {
+            if(ch == str.charAt(i)) 
+            {
+                ++frequency;
+            }
+        }
+
+        System.out.println("Frequency of " + ch + " = " + frequency);
+    }
+}
