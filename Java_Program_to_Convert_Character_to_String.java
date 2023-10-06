774dbb0c2ed3bcca3b6ec9cbe22e70c8bff47ff4
ananya990 <142164935+ananya990@users.noreply.github.com>
2023-10-06 19:20:55 +0530

    Create Java_Program_to_Convert_Character_to_String.java

---
+++ Java_Program_to_Convert_Character_to_String.java
@@ -0,0 +1,10 @@
+public class CharString 
+{
+    public static void main(String[] args) 
+    {
+        char ch = 't';
+        String st = Character.toString(ch);
+
+        System.out.println("The string is : " + st);
+    }
+}
