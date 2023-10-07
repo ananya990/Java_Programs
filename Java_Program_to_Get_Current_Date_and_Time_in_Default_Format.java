3a2113ce7fcc991457a3fa051acc073e9ca412b3
ananya990 <142164935+ananya990@users.noreply.github.com>
2023-10-07 09:37:10 +0530

    Create Java_Program_to_Get_Current_Date_and_Time_in_Default_Format.java

---
+++ Java_Program_to_Get_Current_Date_and_Time_in_Default_Format.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+
+public class CurrentDateTime 
+{
+    public static void main(String[] args) 
+    {
+        LocalDateTime current = LocalDateTime.now();
+
+        System.out.println("Current Date and Time : " + current);
+    }
+}
