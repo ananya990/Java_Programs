f0783204b4e18fb4db1d46d8c6fbf4dac587690b
ananya990 <142164935+ananya990@users.noreply.github.com>
2023-10-22 19:02:48 +0530

    Create Java_Program_to_Append_Text_to_an_Existing_File_Using_FileWriter.java

---
+++ Java_Program_to_Append_Text_to_an_Existing_File_Using_FileWriter.java
@@ -0,0 +1,21 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class AppendFile 
+{
+    public static void main(String[] args) 
+    {
+        String path = System.getProperty("user.dir") + "\\src\\test.txt";
+        String text = "This line is added into the file.";
+
+        try 
+        {
+            FileWriter fw = new FileWriter(path, true);
+            fw.write(text);
+            fw.close();
+        }
+          
+        catch(IOException e) 
+        { }
+    }
+}
